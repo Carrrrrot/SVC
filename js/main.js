9f8103f6ee9e1597c1d6bcfd330bcab34f0da180
ZaikinAndrii <glescrawl@gmail.com>
2015-06-27 02:20:06 +0300

    Finished about-school page.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
-
+jQuery(document).ready(function($) {
+    $(".scroll").click(function(event){
+        event.preventDefault();
+        $('html,body').animate({scrollTop:$(this.hash).offset().top}, 1000);
+    });
+});
 
         $('.subject').selectize({
     	create: true,
     	sortField: 'text'
 		});
-       
 
-  $('[data-toggle="tooltip"]').tooltip()
+
+  $('[data-toggle="tooltip"]').tooltip();
