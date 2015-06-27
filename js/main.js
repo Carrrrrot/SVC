9b3592ec225470bef828e99fe1994ddfa7cb9edd
ZaikinAndrii <glescrawl@gmail.com>
2015-06-27 10:59:56 +0300

    FIXES!

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
+jQuery(document).ready(function($) {
+    $(".scroll").click(function(event){
+        event.preventDefault();
+        $('html,body').animate({scrollTop:$(this.hash).offset().top}, 1000);
+    });
+});
+
+$(document).click(function() {
+    $(".collapse").removeClass("in");
+});
 
 
         $('.subject').selectize({
-    	create: true,
-    	sortField: 'text'
-		});
-       
+        create: true,
+        sortField: 'text'
+        });
 
-  $('[data-toggle="tooltip"]').tooltip()
+
+  $('[data-toggle="tooltip"]').tooltip();
