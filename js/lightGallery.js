7fcaa97223bc4349aad0e01d5351ebf5f5841713
lena levenok <dzvinka3333@gmail.com>
2015-07-10 08:50:30 +0300

    light

--- js/lightGallery.js
+++ js/lightGallery.js
@@ -37,7 +37,7 @@
 
                 exThumbImage: false,
                 thumbnail: true,
-                showThumbByDefault: false,
+                showThumbByDefault: true,
                 animateThumb: true,
                 currentPagerPosition: 'middle',
                 thumbWidth: 100,
